2b7f9bbe4deab73ea8ca0904d8e4f169042e5e45
Stacy985 <anastasiya.disy@gmail.com>
2024-04-12 20:42:09 +0300

    js_cards

--- general/js/index.js
+++ general/js/index.js
@@ -3,68 +3,62 @@ function workPopup(popup, param) {
   popup.classList.remove(param[1]); //удалить
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  let scheduleDays = [
-    {
-      title: "Понедельник 15.04.2024",
-      listLesson: "1",
-      today:"20240415"
-    },
-    {
-      title: "Воскресенье 16.04.2024",
-      listLesson: "2",
-      today:"20240416"
-    },
-  ];
+function createElementScheduleDays(direction){
+  let numDay = 0;
+  let countDay = 0;
+  let start = 0;
+  let stop = 4;
+  let elem = 0;
+  let allCountDay = scheduleDays.length - 1;
+if (direction == undefined){
+  // first start
+  //дата время поставить
+  //получить текущее время(рассчитать старт исходя из текущего дня)
+  }
+  else{
+  let timingDay = document.querySelectorAll("._timing_day");
+    if(direction == "left"){
+    elem = parseInt(timingDay[0].dataset["id_day"], 10);
+    start = elem - 5;
+      if(start < 0){
+      start = 0;
+      }
+    }
+    else {
+    elem = parseInt(timingDay[timingDay.length - 1].dataset["id_day"], 10);
+    start = elem + 1;
+      if (timingDay.length - 1 <= 3 )  {
+        return;
+      }
+    }
+    if(start > allCountDay){
+      start = allCountDay - 5;
+    }
+  stop = start + 4;
+  document.querySelector("._timing_info").innerHTML = "";
 
-  let scheduleLesson = {
-    1: [
-      {
-        time: "12:30 - 14:00",
-        location: "Бауманская",
-        locationNum : 1,
-        type: "Йогатерапия 2 уровень",
-        commemt: "Замена",
-      },
-      {
-        time: "15:00 - 16:30",
-        location: "Беляево",
-        locationNum : 2,
-        type: "Йогатерапия 1 уровень",
-        commemt: "урок",
-      },
-    ],
-  2: [
-      {
-        time: "12:30 - 14:00",
-        location: "Бауманская",
-        locationNum : 1,
-        type: "Йогатерапия 2 уровень",
-        commemt: "Замена",
-      },
-      {
-        time: "15:00 - 16:30",
-        location: "Беляево",
-        locationNum : 2,
-        type: "Йогатерапия 1 уровень",
-        commemt: "урок",
-      },
-    ],
-  };
-  let timegInfo = document.querySelector("._timing_info");
-  for (let day of scheduleDays) {
+  }
+
+console.log("start: "+start+" stop: "+stop);
+
+
+  for(let day of scheduleDays) {
+    if(numDay >= start && numDay <= stop){
     let dayElem = document.createElement("div");
     dayElem.classList.add("_timing_day");
+    dayElem.dataset["id_day"] = numDay;
 
     let dayName = document.createElement("div");
     dayName.classList.add("_timing_day-name");
     dayName.textContent = day["title"];
+    //присвоить класс если это текуший день
 
     let dayList = document.createElement("div");
     dayList.classList.add("_timing_day-list");
 
     if (scheduleLesson[day["listLesson"]]) {
       for (let less of scheduleLesson[day["listLesson"]]) {
+
         let lessElem = document.createElement("div");
         lessElem.classList.add("_timing_day-list-class");
 
@@ -95,18 +89,131 @@ document.addEventListener("DOMContentLoaded", function () {
 
     dayElem.append(dayName);
     dayElem.append(dayList);
-    timegInfo.append(dayElem);
+    document.querySelector("._timing_info").append(dayElem);
+    countDay++;
+      if(countDay == 5){
+      break;
+      }
+    }
+    numDay++;
   }
-  document.querySelector("._timing-select-cont > select").addEventListener("change", function () {
+}
+document.addEventListener("DOMContentLoaded", function () {
+
+  window.scheduleDays = [
+    {
+      title: "Понедельник1 15.04.2024",
+      listLesson: "1",
+      day: "20240415",
+    },
+    {
+      title: "Воскресенье2 16.04.2024",
+      listLesson: "2",
+      day: "20240416",
+    },
+    {
+      title: "Воскресенье3 16.04.2024",
+      listLesson: "2",
+      day: "20240416",
+    },
+    {
+      title: "Воскресенье4 16.04.2024",
+      listLesson: "2",
+      day: "20240416",
+    },
+    {
+      title: "Воскресенье5 16.04.2024",
+      listLesson: "2",
+      day: "20240416",
+    },
+    {
+      title: "Воскресенье6 16.04.2024",
+      listLesson: "2",
+      day: "20240416",
+    },
+    {
+      title: "Вторник7 16.04.2024",
+      listLesson: "2",
+      day: "20240416",
+    },
+    {
+      title: "Вторник8 16.04.2024",
+      listLesson: "2",
+      day: "20240416",
+    },
+    
+  ];
+
+  window.scheduleLesson = {
+    1: [
+      {
+        time: "12:30 - 14:00",
+        location: "Бауманская",
+        locationNum: 1,
+        type: "Йогатерапия 2 уровень",
+        commemt: "Замена",
+      },
+      {
+        time: "15:00 - 16:30",
+        location: "Беляево",
+        locationNum: 2,
+        type: "Йогатерапия 1 уровень",
+        commemt: "урок",
+      },
+    ],
+    2: [
+      {
+        time: "12:30 - 14:00",
+        location: "Бауманская",
+        locationNum: 1,
+        type: "Йогатерапия 2 уровень",
+        commemt: "Замена",
+      },
+      {
+        time: "15:00 - 16:30",
+        location: "Беляево",
+        locationNum: 2,
+        type: "Йогатерапия 1 уровень",
+        commemt: "урок",
+      },
+    ],
+  };
+  createElementScheduleDays();
+
+  document
+    .querySelector("._timing-select-cont > select")
+    .addEventListener("change", function () {
       console.log(this.value);
     });
 
-    document.querySelector("._timing_button-left").addEventListener("click", function () {
+  document
+    .querySelector("._timing_button-left")
+    .addEventListener("click", function () {
       console.log("left");
     });
 
-    document.querySelector("._timing_button-right").addEventListener("click", function () {
+  document
+    .querySelector("._timing_button-right")
+    .addEventListener("click", function () {
       console.log("right");
+    });
+
+/*     function scrollPage(direction) {
+      const scrollDistance = 220; 
+      const scrollDirection = direction === "left" ? -scrollDistance : scrollDistance;
+      document.querySelector("._timing_info").scrollBy({
+        top: 0,
+        left: scrollDirection,
+        behavior: "smooth" // Плавная прокрутка
+      });
+    } */
+    
+    document.querySelector("._timing_button-left").addEventListener("click", function () {
+      createElementScheduleDays("left");
+    });
+    
+    document.querySelector("._timing_button-right").addEventListener("click", function () {
+      createElementScheduleDays("right");
     });
 
 
